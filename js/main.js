3a9e6d76fa062cdd3445c4a2f92936a1ed31193b
Kathryn McPhee <exverne@hotmail.com>
2017-09-22 18:20:53 +1000

    Added javascript

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Load the Document
+$(document).ready(function(){
+
+  //Log
+  console.log('ready!');
+
+// click the hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+    // shows the mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+  });
+  $('#mobile-menu a').click(function(){
+
+    // shows the mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+  });
+
+
+//clicking the cross icon
+
+//close the mobile menu
+});
